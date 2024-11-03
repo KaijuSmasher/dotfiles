408f40950c8b213c074f5aedb3800af8d43c1615
kaijusmasher <kaijusmasher1998@gmail.com>
2024-11-03 06:36:59 +0100

    First commit for uploading

---
+++ nvim/lua/configs/conform.lua
@@ -0,0 +1,39 @@
+local options = {
+    formatters_by_ft = {
+        lua = { "stylua" },
+        c = { "clang-format" },
+        cpp = { "clang-format" },
+        python = { "black" },
+    },
+
+    formatters = {
+        --C and CPP
+        ["clang-format"] = {
+            prepend_args = {
+                "-style={ \
+                IndentWidth: 4, \
+                TabWidth: 4, \
+                UseTab: Never, \
+                AccessModifierOffset: 0, \
+                IndentAccessModifiers: true, \
+                PackConstructorInitializers: Never}",
+            },
+        },
+        --Python
+        black = {
+            prepend_args = {
+                "--fast",
+                "--line-length",
+                "80",
+            },
+        },
+    },
+
+    format_on_save = {
+        -- These options will be passed to conform.format()
+        timeout_ms = 500,
+        lsp_fallback = true,
+    },
+}
+
+return options
---
+++ nvim/lua/configs/treesitter.lua
@@ -0,0 +1,30 @@
+--Add additional file types here
+local options = {
+    ensure_installed = {
+        "bash",
+        "c",
+        "cmake",
+        "cpp",
+        "fish",
+        "lua",
+        "luadoc",
+        "make",
+        "markdown",
+        "printf",
+        "python",
+        "rust",
+        "toml",
+        "vim",
+        "vimdoc",
+        "yaml",
+    },
+
+    highlight = {
+        enable = true,
+        use_languagetree = true,
+    },
+
+    indent = { enable = true },
+}
+
+require("nvim-treesitter.configs").setup(options)
